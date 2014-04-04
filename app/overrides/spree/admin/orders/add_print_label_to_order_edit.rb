2c5614170436633171b6e71ce5a5330ccc6f5a53
Seb Brocher <seb@noctual.com>
2014-04-04 15:12:16 -0600

    updated to work with deface 1.0

--- app/overrides/spree/admin/orders/add_print_label_to_order_edit.rb
+++ app/overrides/spree/admin/orders/add_print_label_to_order_edit.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path  => "spree/admin/orders/_shipment",
                      :name          => "add_print_label_to_order_edit",
-                     :insert_before  => "code[erb-loud]:contains(\"link_to 'ship', '#'\")",
+                     :insert_before => "erb[loud]:contains(\"ship button icon-arrow-right\")",
                      :partial       => "spree/admin/orders/label")
